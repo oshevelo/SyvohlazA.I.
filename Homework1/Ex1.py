83b33ebdfb854a6c5dfadf5136ef253b96117e06
jonnytaddesky <s.v.artem2020@gmail.com>
2019-09-23 12:20:59 +0300

    try except simple calculator

--- Homework1/Ex1.py
+++ Homework1/Ex1.py
@@ -1,23 +1,32 @@
-def calculator(operand1, operand2, operation):
-    if operation == '+':
-        print('result =', operand1 + operand2)
-    elif operation == '-':
-        print('result =', operand1 - operand2)
-    elif operation == '*':
-        print('result =', operand1 * operand2)
-    elif operation == '/':
-        if operand2 == 0:
-            print('Divided by Zero')
+def calculator(operand1, operand2, operation='+'):
+    try:
+        operand1, operand2 = float(operand1), float(operand2)
+    except ValueError:
+        return 'Input error: value must be float numbers'
+    else:
+        if operation == '+':
+            print('result =', operand1 + operand2)
+        elif operation == '-':
+            print('result =', operand1 - operand2)
+        elif operation == '*':
+            print('result =', operand1 * operand2)
+        elif operation == '**':
+            print('result =', operand1 ** operand2)
+        elif operation == '/':
+            if operand2 == 0:
+                return 'Division by zero'
+            else:
+                print('result =', operand1 / operand2)
         else:
-            print('result =', operand1 / operand2)
+            print('Wrong symbol operation')
 
 
-print('Eneter operand1: ')
-operand1 = float(input())
-print('Eneter operation: ')
-operation = input()
-print('Eneter operand2: ')
-operand2 = float(input())
+operand1 = input('Enter operand1: ')
+operation = input('Enter operation: ')
+operand2 = input('Enter operand2: ')
 
+result = calculator(operand1=operand1,
+                    operand2=operand2, operation=operation)
 
-calculator(operand1, operand2, operation)
+if isinstance(result, str):
+    print(result)
