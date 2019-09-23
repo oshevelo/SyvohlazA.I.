276ceae5191f1b6b7dbc1188dbed22502d765e2b
jonnytaddesky <s.v.artem2020@gmail.com>
2019-09-23 12:21:58 +0300

    calculator of medim, no order of operation,i will finish

---
+++ Homework1/Ex2.py
@@ -0,0 +1,64 @@
+def calculate(operand1, operand2, operation='+'):
+    try:
+        operand1, operand2 = float(operand1), float(operand2)
+    except ValueError:
+        return 'Input error'
+    else:
+        if operation == '+':
+            return operand1 + operand2
+        elif operation == '-':
+            return operand1 - operand2
+        elif operation == '*':
+            return operand1 * operand2
+        elif operation == '/':
+            if operand2 == 0:
+                return 'Division by zero'
+            else:
+                return operand1 / operand2
+        else:
+            if operation in ('+', '-', '*', '/', '**'):
+                return operation
+            else:
+                return 'Wrong symbol'
+
+
+count = input('Enter number values: ')
+
+try:
+    count = int(count)
+except ValueError:
+    print('Value must be integers')
+else:
+    if count == 1:
+        operand1 = input('Enter operand1: ')
+        print(operand1)
+    elif count > 0:
+        i = 2
+
+        operand1 = input('Enter operand1: ')
+        operation = input('Enter operation: ')
+        operand2 = input('Enter operand2: ')
+
+        result = calculate(operand1=operand1,
+                           operand2=operand2, operation=operation)
+
+        if isinstance(result, str):
+            print(result)
+        else:
+            while i < count:
+                operand1 = result
+                operation = input('Enter next operations: ')
+                operand2 = input('Enter next operand: ')
+                if isinstance(result, str):
+                    break
+                if operation not in ('+', '-', '*', '/', '**'):
+                    break
+
+                result = calculate(operand1=operand1,
+                                   operand2=operand2, operation=operation)
+
+                i += 1
+
+            print(result)
+    else:
+        print('Nothing to calculate')
