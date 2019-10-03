393d211065e85e13a5d91d5c2c41db9d020d0757
jonnytaddesky <s.v.artem2020@gmail.com>
2019-10-03 17:18:06 +0300

    add pi and e const

--- Homework1/Ex3.py
+++ Homework1/Ex3.py
@@ -3,12 +3,15 @@ from simpleeval import simple_eval
 def calculate_string(s):
     try:
        s = simple_eval(s)
-    except Exception:
-        return 'Wrong input'
+    except Exception as e:
+        return e
     else:
         return s
             
 s = input('Input expression: ')
+
+s = s.replace('pi', '3.14')
+s = s.replace('e', '2.71')
 
 print('result =', calculate_string(s))
 
